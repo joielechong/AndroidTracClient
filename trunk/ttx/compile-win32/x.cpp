b85807d5b01e6497df85f89290c68a0f786b7387
nobody <nobody@22849511-53eb-4f63-ad75-5235c46a5120>
2009-05-27 21:58:29 +0000

    Initial commit

---
+++ trunk/ttx/compile-win32/x.cpp
@@ -0,0 +1,46 @@
+ /* confdefs.h.  */
+ 
+ #define PACKAGE_NAME ""
+ #define PACKAGE_TARNAME ""
+ #define PACKAGE_VERSION ""
+ #define PACKAGE_STRING ""
+ #define PACKAGE_BUGREPORT ""
+ #define PACKAGE "ttx"
+ #define VERSION "2.4.8pre"
+ #ifdef __cplusplus
+ extern "C" void std::exit (int) throw (); using std::exit;
+ #endif
+ #define PROTOTYPES 1
+ #define __PROTOTYPES 1
+ #define STDC_HEADERS 1
+ #define HAVE_SYS_TYPES_H 1
+ #define HAVE_SYS_STAT_H 1
+ #define HAVE_STDLIB_H 1
+ #define HAVE_STRING_H 1
+ #define HAVE_MEMORY_H 1
+ #define HAVE_STRINGS_H 1
+ #define HAVE_INTTYPES_H 1
+ #define HAVE_STDINT_H 1
+ #define HAVE_UNISTD_H 1
+ #define HAVE_STDIO_H 1
+ #define HAVE_STRING_H 1
+ #define HAVE_SYS_TYPES_H 1
+ #define HAVE_ERRNO_H 1
+ #define HAVE_UNISTD_H 1
+ #define HAVE_STDLIB_H 1
+ #define HAVE_GETOPT_H 1
+ #define HAVE_WINSOCK2_H 1
+ #define HAVE_WINSOCK_H 1
+ #define HAVE_WS2TCPIP_H 1
+ #define HAVE_VPRINTF 1
+ /* end confdefs.h.  */
+
+#include <winsock2.h>
+ /* Override any gcc2 internal prototype to avoid an error.  */
+ int
+ main ()
+ {
+ gethostbyname ("");
+   ;
+   return 0;
+ }
