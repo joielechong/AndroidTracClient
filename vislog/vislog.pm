7272b3308f0ed5482b4a5000d29d526e14c64993
nobody <nobody@22849511-53eb-4f63-ad75-5235c46a5120>
2009-05-27 15:24:03 +0000

    Wijizginging uit okt 2006 lijkt het

--- vislog/vislog.pm
+++ vislog/vislog.pm
@@ -7,83 +7,88 @@ use XML::Simple;
 
 sub vislog {
 
-$Data::Dumper::Indent=1;
+   $Data::Dumper::Indent=1;
 
-my $xs = new XML::Simple;
-my $asn = Convert::ASN1->new;
-my %xml;
+   my $xs = new XML::Simple;
+   my $asn = Convert::ASN1->new;
+   my %xml;
 
-$asn->prepare_file("vptbev.txt");
+   $asn->prepare_file("vptbev.txt");
 
-my $asn1=$asn->find("VPT-Bev21-Message");
-# $asn1->dump;
+   my $asn1=$asn->find("VPT-Bev21-Message");
+#   $asn1->dump;
 
-my $file = shift;
+   my $file = shift;
     
-open VL,$file or die "Kan file $file niet openen\n";
-my $start=shift;
-my $end=shift;
-$start=0 unless $start;
-$end=999999999 unless $end;
-my $lc=0;
+   open VL,$file or die "Kan file $file niet openen\n";
+#   open DEB,">$file.txt" or die "Kan file $file.txt niet openen\n";
+   my $start=shift;
+   my $end=shift;
+   $start=0 unless $start;
+   $end=999999999 unless $end;
+   my $lc=0;
 
-while () {
-    my $dt="";
-    my $type=0;
-    my $hent="";
-    my $c;
-    my $ncs="";
+   while () {
+      my $dt="";
+      my $type=0;
+      my $hent="";
+      my $c;
+      my $ncs="";
     
-    $lc++;
-    last unless read(VL,$c,1);
-    while ( $c ne ";") {
-	$dt = $dt.$c;
-	exit unless read(VL,$c,1);
-    }
-    read(VL,$c,1);
-    $type=$c;
-    if ($type eq "0") {
-	readline(*VL);
-    } else {
-	read(VL,$c,1);
-	read(VL,$c,1);
-	while ( $c ne ";") {
+      $lc++;
+      last unless read(VL,$c,1);
+      while ( $c ne ";") {
+	 $dt = $dt.$c;
+	 exit unless read(VL,$c,1);
+      }
+      read(VL,$c,1);
+      $type=$c;
+      if ($type eq "0") {
+	 readline(*VL);
+      } else {
+ 	 read(VL,$c,1);
+	 read(VL,$c,1);
+	 while ( $c ne ";") {
 	    $hent = $hent.$c;
 	    read(VL,$c,1);
-	}
-	read(VL,$c,1);
-	while ( $c ne ";") {
+	 }
+	 read(VL,$c,1);
+	 while ( $c ne ";") {
 	    $ncs = $ncs.$c;
 	    read(VL,$c,1);
-	}
-	my $nc=read(VL,$buffer,$ncs);
-	if ($lc >= $start && $lc <= $end){
+	 }
+	 my $nc=read(VL,$buffer,$ncs);
+	 if ($lc >= $start && $lc <= $end){
 	    my $out = $asn1->decode($buffer,{time=>'raw'});
 	    if ($out) {
-		my %out=%{$out};
-		unless (
-			exists $out{'vptprl-bev21-live-check-message'} ||
-			exists $out{'vptprl-bev21-live-check-response'} ) {   
-#		    print "$file $lc $dt $hent ",join(",",keys(%out)),"\n";
-#		    print Dumper($out);
-		    $xml{sprintf("%08d",$lc)}={'ts' => $dt, 'ip' => $hent, %out};
-		}
-	    } else {
+	       my %out=%{$out};
+               if (exists $out{'occupation-report'} ||
+                   exists $out{'route-report'} ) {
+#	 	   print DEB "File = $file\nlc = $lc\ndt = $dt\nhent = $hent\nKey = ",join(",",keys(%out)),"\n";
+#		   print DEB Dumper($out);
+               }
+	       unless (
+		  exists $out{'vptprl-bev21-live-check-message'} ||
+		  exists $out{'vptprl-bev21-live-check-response'} ) {   
+		   $xml{sprintf("%08d",$lc)}={'ts' => $dt, 'ip' => $hent, %out};
+	       }
+  	     } else {
 		print "$file $lc $dt error:",$asn1->error,"\n";
 		$xml{sprintf("%08d",$lc)}={'ts' => $dt, 'ip' => $hent, 'asn1_error' =>  {error_message => $asn1->error}};
-	    }
-	}
-	read(VL,$c,1);
-    }
-}
+	     }
+	  }
+	  read(VL,$c,1);
+      }
+   }
 
-close VL;
+   close VL;
+#   close DEB;
 
-my %all;
-$all{"msg"}=\%xml;
+   my %all;
+   $all{"msg"}=\%xml;
 
-my $logfile=$file.".xml";
-my $xml=$xs->XMLout(\%all,RootName=>'VisLog',OutPutFile=>"$logfile",KeyAttr=>'nr');
+   my $logfile=$file.".xml";
+   my $xml=$xs->XMLout(\%all,RootName=>'VisLog',OutPutFile=>"$logfile",KeyAttr=>'nr');
 
 
 }  
