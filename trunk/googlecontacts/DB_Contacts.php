5a2e1fab4b75b7e4a45bc812b718b4b877b023a5
mfvl <mfvl@22849511-53eb-4f63-ad75-5235c46a5120>
2009-12-07 22:54:29 +0000

    Split into separate files

---
+++ trunk/googlecontacts/DB_Contacts.php
@@ -0,0 +1,55 @@
+<?php
+require_once 'Contacts.php'
+
+class DB_Contacts  extends Contacts {
+  private $entry;
+  private $tabel = array('Mobiel' => 'mobile',
+			 'Werk' => 'work',
+			 'Prive' => 'home',
+			 'Anders' => 'other'
+			 );
+    
+  public function __construct($entry) {
+	$this->entry = $entry;
+  }
+  private function generic_array($d,$f1,$f2) {  
+    $a=array();
+    foreach($d as $e) {
+	  $t = $e[$f1];
+	  if(isset($this->tabel[$t])) {
+	    $t = $this->tabel[$t];
+	  }
+	  if ($e['class'] === 'FAX') {
+	    $t = $t."_fax";
+	  }
+	//echo "<!-- ".$e[$f1]." $t -->\n";
+	  $a[] = $t.": ".$e[$f2];
+    }
+	return $a;
+  }
+  public function getName() {
+    return utf8_decode($this->entry->contact['cn']);
+  }
+  public function getOrgName() {
+    return utf8_decode($this->entry->contact['company']);
+  }
+  public function getMail() {
+    return  $this->generic_array($this->entry->mail,'type','mailaddress');
+  }
+  public function getPhoneNumber() {
+    return  $this->generic_array($this->entry->phone,'tel_type','number');
+  }
+  public function getWebsite() {
+    return  $this->generic_array($this->entry->web,'type','webpagina');
+  }
+  public function getAddress() {
+    return  $this->generic_array($this->entry->naw,'adr_type','adres');
+  }
+  public function getTime() {
+    return strtotime($this->entry->contact['updatetime']);
+ }
+  public function getId() {
+    return $this->entry->contact['id'];
+ }
+}
+?>
---
+++ trunk/googlecontacts/GMail_Contacts.php
@@ -0,0 +1,103 @@
+<?php
+require_once 'Contacts.php'
+
+class GMail_Contacts extends Contacts {
+  private $entry;
+  private $xml;
+  
+  public function getName() {
+    if (empty($this->entry->title)) {
+	  return FALSE;
+	}
+    return (string) $this->entry->title;
+  }
+  public function getContent() {
+    return (string) $this->entry->content;
+  }
+  public function getId() {
+    if (isset($this->entry->content) && strstr($this->entry->content,"=") != FALSE) {
+      list($key,$val) = explode('=',$this->entry->content);                                     
+      if (isset($key) && ($key === "id")) {
+	    return $val;
+      }
+    }
+	return -1;
+  }
+  public function getUpdated() {
+    return (string) $this->entry->updated;
+  }
+  public function getTime() {
+    return strtotime($this->entry->updated);
+  }
+  public function getOrgName() {
+    return (string) $this->xml->organization->orgName;
+  }
+  public function getOrgTitle() {
+    return (string) $this->xml->organization->orgTitle;
+  }
+  public function getFullName() {
+    return (string) $this->xml->fullName;
+  }
+  public function getGivenName() {
+    return (string) $this->xml->givenName;
+  }
+  public function getAdditionalName() {
+    return (string) $this->xml->additionalName;
+  }
+  public function getFamilyName() {
+    return (string) $this->xml->familyName;
+  }
+  public function getMail() {
+    $emailAddress = array();
+    foreach ($this->xml->email as $e) {
+      $relstr = (string) $e['rel'];
+      if (strstr($relstr,"#") != FALSE) {
+	list($g,$rel) = explode("#",$relstr);
+      } else {
+	$rel = "??";
+      }
+      $emailAddress[] =  $rel.": ".(string) $e['address'];
+    }  
+    return $emailAddress;
+  }
+  public function getPhoneNumber() {
+    $phoneNumber = array();
+    foreach ($this->xml->phoneNumber as $p) {
+      $relstr = (string) $p['rel'];
+      if (strstr($relstr,"#") != FALSE) {
+	list($g,$rel) = explode("#",$relstr);
+      } else {
+	$rel = "mobile";
+      }
+      $phoneNumber[] = $rel.": ".(string) $p;
+    }
+	return $phoneNumber;
+  }
+  public function getAddress () {
+    $Address = array();
+    foreach ($this->xml->structuredPostalAddress as $a) {
+      $relstr = (string) $a['rel'];
+      if (strstr($relstr,"#") != FALSE) {
+	list($g,$rel) = explode("#",$relstr);
+      } else {
+	$rel = "other";
+      }
+      $Address[] = $rel.": ".(string) $a->formattedAddress;
+    }
+    return $Address;
+  }
+  public function getWebsite() {
+    $website = array();
+    foreach ($this->xml->website as $w) {
+      $rel = (string) $w['rel'];
+      $website[] = $rel .": ".(string) $w['href'];
+    }
+	return $website;
+  } 
+ 
+  public function __construct($entry) {
+	$this->entry = $entry;
+	$this->xml = simplexml_load_string($entry->getXML());
+  }
+}
+?>
