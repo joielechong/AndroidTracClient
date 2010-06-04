d2b46018c545d138e7febac898f0860f2e076f73
mfvl <mfvl@22849511-53eb-4f63-ad75-5235c46a5120>
2010-06-04 08:29:45 +0000

    ''

---
+++ trunk/osm/routeinfo.h
@@ -0,0 +1,35 @@
+#ifndef ROUTEINFO_H
+#define ROUTEINFO_H 1
+
+#include <string>
+#include <stdbool.h>
+
+using namespace std;
+
+class location {
+
+ public:
+  inline location() :node(false), coord(false), address(false) {}
+
+  bool node;
+  long nodeid;
+
+  bool coord;
+  double lat,lon;
+
+  bool address;
+  string country,city,street,number,postcode;
+};
+
+class routeinfo {
+ public: 
+  string vehicle;
+  location locations[2];
+};
+
+extern routeinfo inforoute;
+void routelex_init(const char *s);
+int yyparse(void);
+
+
+#endif
