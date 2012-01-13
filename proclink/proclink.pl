facdb80ce5a47d280eb8d5452898326c93770b0f
mfvl <mfvl@22849511-53eb-4f63-ad75-5235c46a5120>
2012-01-13 12:08:43 +0000

    restore update executable pl files that were only updated in the bin directories

--- proclink/proclink.pl
+++ proclink/proclink.pl
@@ -3,10 +3,8 @@
 use strict;
 use DBI;
 
-my $dbh=DBI::connect("dbi:Pg:dbname=fietspad") or &error;
-
-my $sthup=$dbh->prepare("UPDATE content SET calls=calls+1,lastcal=?,last_visitor=? WHERE url=?");
-my $sthin=$dbh->prepare("INSERT INTO content (url,calls,last_visitor,last_call) VALUES(?,1,?,?);
+my $dbh = DBI->connect("dbi:Pg:dbname=fietspad");
+my $sthupd = $dbh->prepare("SELECT addcall(?,?,?)");
 
 while (1==1) {
     
@@ -15,27 +13,15 @@ while (1==1) {
     while (<LINK>) {
 #    print STDERR ;
 	
-	my ($host, $date,$url) = /^([0-9\.]+ )?\[(.*)\] \/linkhttp\/(.*)$/;
+	my ($host, $date,$url) = /^([a-f0-9\.:]+ )?\[(.*)\] \/linkhttp\/(.*)$/;
 	$host="-" unless $host;
         $host=~s/ *$//;
+#    print STDERR $date, "-", $url,"\n";
+	next unless defined($url);
+	next if $url eq "";
+	
 	$url = "http://".$url;
-#    print STDERR $date, "-", $url,"-",$host"\n";
-my $result=$sthup->execute($url,$host,$date);
-
-	my $count = $result->rows;
-	
-#    print STDERR "UPDATE 1 url = $url, count = $count\n";
-	
-	if ($count == 0) {
-	    my $url1 = $url."/";
-	    $result = $sthup->update($url1,$host,$date);
-	    my $count = $result->rows;
-#    print STDERR "UPDATE 2 url = $url, count = $count\n";
-	    if ($count == 0) {
-	    $result=$sthin($host,$date,$url);
-#	print STDERR "INSERT result = ",$result->rows\n";
-	    }
-	}
+	$sthupd->execute($date,$host,$url);
     }
     
     close LINK;
