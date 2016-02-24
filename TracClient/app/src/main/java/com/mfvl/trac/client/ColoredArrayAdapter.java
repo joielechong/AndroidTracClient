5d5832cd7a2dc2e30ec55520503329056aae931c
mfvl <mfvl@22849511-53eb-4f63-ad75-5235c46a5120>
2016-02-24 02:05:51 +0000

    Nightly run 0.6.2-rc6

--- TracClient/app/src/main/java/com/mfvl/trac/client/ColoredArrayAdapter.java
+++ TracClient/app/src/main/java/com/mfvl/trac/client/ColoredArrayAdapter.java
@@ -20,7 +20,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class ColoredArrayAdapter<T> extends ArrayAdapter<T> {
