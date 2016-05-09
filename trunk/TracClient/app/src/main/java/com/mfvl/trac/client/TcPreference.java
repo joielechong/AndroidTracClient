86086bd523ae126f5e1c9b221dfa3ede829e8015
mfvl <mfvl@22849511-53eb-4f63-ad75-5235c46a5120>
2016-05-09 01:05:20 +0000

    Nightly run 0.7-rc10

--- trunk/TracClient/app/src/main/java/com/mfvl/trac/client/TcPreference.java
+++ trunk/TracClient/app/src/main/java/com/mfvl/trac/client/TcPreference.java
@@ -1,29 +1,49 @@
 package com.mfvl.trac.client;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.widget.Button;
+import android.preference.PreferenceScreen;
 
 import com.mfvl.mfvllib.MyLog;
 
-import java.util.List;
-
 public class TcPreference extends PreferenceActivity {
+
     @Override
     protected boolean isValidFragment(String frag) {
         MyLog.d(frag);
         return true;
     }
 
-   @SuppressWarnings("unused")
-   public static class SettingsFragment extends PreferenceFragment {
+    @SuppressWarnings("unused")
+    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             MyLog.i("Arguments: " + getArguments());
+            getPreferenceManager().setSharedPreferencesName(Const.PREFS_NAME);
+            getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
             addPreferencesFromResource(R.xml.preferences);
+            EditTextPreference pref = (EditTextPreference)findPreference(getString(R.string.prefNrItemsKey));
+            String val = pref.getText();
+            pref.setSummary(val);
         }
-    }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            MyLog.d(key);
+            Preference pref = findPreference(key);
+
+            if (pref instanceof EditTextPreference) {
+                EditTextPreference editPref = (EditTextPreference) pref;
+                String val = editPref.getText();
+                pref.setSummary(val);
+                TracGlobal.ticketGroupCount = Integer.parseInt(val);
+                MyLog.d("val = "+val);
+            }
+        }    }
 
 }
