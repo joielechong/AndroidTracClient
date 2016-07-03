bfe50dfd893fd8eb66c1927dd7fd350bd5879d2d
mfvl <mfvl@22849511-53eb-4f63-ad75-5235c46a5120>
2016-07-03 22:17:02 +0000

    Copyright message standardized

--- TracClient/app/src/main/java/com/mfvl/trac/client/SortSpec.java
+++ TracClient/app/src/main/java/com/mfvl/trac/client/SortSpec.java
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-package com.mfvl.trac.client;
+ */com.mfvl.trac.client;
 
 public class SortSpec extends SpecImpl {
     private boolean _richting; // true = asc false = desc
--- TracClient/app/src/main/java/org/alexd/jsonrpc/JSONRPCException.java
+++ TracClient/app/src/main/java/org/alexd/jsonrpc/JSONRPCException.java
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-package org.alexd.jsonrpc;
+ */org.alexd.jsonrpc;
 
 /**
  * Represents an error during JSON-RPC method call. Various reasons can make a JSON-RPC call fail (network not available, non
