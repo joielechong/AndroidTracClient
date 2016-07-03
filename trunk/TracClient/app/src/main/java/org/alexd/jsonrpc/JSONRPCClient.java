a61da2799e9c22774d1197cf09277281d81c2a4e
mfvl <mfvl@22849511-53eb-4f63-ad75-5235c46a5120>
2016-07-03 22:34:38 +0000

    Roll back to 2021

--- trunk/TracClient/app/src/main/java/org/alexd/jsonrpc/JSONRPCClient.java
+++ trunk/TracClient/app/src/main/java/org/alexd/jsonrpc/JSONRPCClient.java
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */org.alexd.jsonrpc;
+ */
+
+package org.alexd.jsonrpc;
 
 import org.alexd.jsonrpc.JSONRPCParams.Versions;
 import org.json.JSONArray;
